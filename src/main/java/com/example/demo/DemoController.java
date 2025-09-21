7080f07150c74e419019ecbe3d344f03ddff31ee
heungkukyee <silkroad_jk@naver.com>
2025-09-22 02:42:45 +0900

    2주차 연습문제 적용(URL 맵핑과 컨트롤러 추가)

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -11,4 +11,14 @@ public class DemoController {
         model.addAttribute("data", " 방갑습니다."); // model 설정
         return "hello"; // hello.html 연결
     }
+
+    @GetMapping("/hello2") 
+    public String hello2(Model model) {
+        model.addAttribute("name", "유장건님.");
+        model.addAttribute("hello", "방갑습니다.");
+        model.addAttribute("today", "오늘.");
+        model.addAttribute("weather", "날씨는.");
+        model.addAttribute("good", "매우 좋습니다."); 
+        return "hello2"; 
+    }
 }
