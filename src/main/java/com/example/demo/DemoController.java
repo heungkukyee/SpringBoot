a1a2d67130ab35bb5411800e5507b9683e41c173
heungkukyee <silkroad_jk@naver.com>
2025-09-22 04:24:05 +0900

    3주차 개인 포트폴리오 설정

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -12,13 +12,18 @@ public class DemoController {
         return "hello"; // hello.html 연결
     }
 
-    @GetMapping("/hello2") 
+    @GetMapping("/hello2")
     public String hello2(Model model) {
         model.addAttribute("name", "유장건님.");
         model.addAttribute("hello", "방갑습니다.");
         model.addAttribute("today", "오늘.");
         model.addAttribute("weather", "날씨는.");
-        model.addAttribute("good", "매우 좋습니다."); 
-        return "hello2"; 
+        model.addAttribute("good", "매우 좋습니다.");
+        return "hello2";
+    }
+
+    @GetMapping("/about_detailed")
+    public String about() {
+        return "about_detailed";
     }
 }
